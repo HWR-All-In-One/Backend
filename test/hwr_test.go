a47453fc5d3fc334f071460a4f43809114a4a25f
Mustafa Hussaini <mustafa.hussaini@etomer.com>
2023-02-03 19:20:44 +0100

    some test coverage

--- test/hwr_test.go
+++ test/hwr_test.go
@@ -21,3 +21,19 @@ func TestValidateUser(t *testing.T) {
 		t.Error("should be valid user but is not")
 	}
 }
+
+func TestIsNotValidUser(t *testing.T) {
+	email := os.Getenv("HWR_EMAIL") + "fail"
+	password := os.Getenv("HWR_PASSWORD") + "fail"
+
+	isValid, err := hwr.ValidateUser(email, password)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if isValid {
+		t.Error("should not be valid user but is")
+	}
+
+}
