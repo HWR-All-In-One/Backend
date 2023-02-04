de2cbbf4263c3aec7755860676a329383f9e0dd5
Bef <bjoerns-software@outlook.de>
2023-02-04 23:08:27 +0100

    Makefile: First steps with bash-script

--- scripts/action_versionnr.sh
+++ scripts/action_versionnr.sh
@@ -2,16 +2,17 @@
 # mainbranch, devbranch, FEATURE_TAG_IN_BRANCH, SUBFEATURE_TAG_IN_BRANCH, BUG_TAG_IN_BRANCH, VERSION_MAIN_MAJOR, VERSION_MAIN_MINOR, VERSION_MAIN_Patch, VERSION_MAIN_BUILD, VERSION_DEV_MAJOR, VERSION_DEV_MINOR, VERSION_DEV_Patch, VERSION_DEV_BUILD, VERSION_DEV_NR
 
 echo "mainbranch: $1";
-echo "devbranch: $2";
-echo "FEATURE_TAG_IN_BRANCH: $3";
-echo "SUBFEATURE_TAG_IN_BRANCH: $4";
-echo "BUG_TAG_IN_BRANCH: $5";
-echo "VERSION_MAIN_MAJOR: $6";
-echo "VERSION_MAIN_MINOR: $7";
-echo "VERSION_MAIN_Patch: $8";
-echo "VERSION_MAIN_BUILD: $9";
-echo "VERSION_DEV_MAJOR: $10";
-echo "VERSION_DEV_MINOR: $11";
-echo "VERSION_DEV_Patch: $12";
-echo "VERSION_DEV_BUILD: $13";
-echo "VERSION_DEV_NR: $14";
+# TODO: Need information about the actual branch and event
+#echo "devbranch: $2";
+#echo "FEATURE_TAG_IN_BRANCH: $3";
+#echo "SUBFEATURE_TAG_IN_BRANCH: $4";
+#echo "BUG_TAG_IN_BRANCH: $5";
+#echo "VERSION_MAIN_MAJOR: $6";
+#echo "VERSION_MAIN_MINOR: $7";
+#echo "VERSION_MAIN_Patch: $8";
+#echo "VERSION_MAIN_BUILD: $9";
+#echo "VERSION_DEV_MAJOR: $10";
+#echo "VERSION_DEV_MINOR: $11";
+#echo "VERSION_DEV_Patch: $12";
+#echo "VERSION_DEV_BUILD: $13";
+#echo "VERSION_DEV_NR: $14";
